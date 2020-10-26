7a8540a35885f9006befc2c9388705fd4ab90892
Thales Correa <thaleshenrique.correa@gmail.com>
2020-10-26 07:19:23 -0300

    nao foi essa caraia

--- app/src/main/java/unis/edu/crudalunos/adapter/CursoAdapter.java
+++ app/src/main/java/unis/edu/crudalunos/adapter/CursoAdapter.java
@@ -1,4 +1,77 @@
 package unis.edu.crudalunos.adapter;
 
-public class CursoAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import unis.edu.crudalunos.R;
+import unis.edu.crudalunos.model.Curso;
+
+public class CursoAdapter extends RecyclerView.Adapter<CursoAdapter.CursoHolder> {
+    private List<Curso> cursos = new ArrayList<>();
+    private OnItemClickListener listener;
+
+    @NonNull
+    @Override
+    public CursoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.curso_item,parent,false);
+        return new CursoAdapter.CursoHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CursoHolder holder, int position) {
+        Curso curso = cursos.get(position);
+        holder.tbCurso.setText(curso.getNome());
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursos.size();
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+        notifyDataSetChanged();
+    }
+
+    public Curso getCursoAt(int position) {
+        return cursos.get(position);
+    }
+
+    class CursoHolder extends RecyclerView.ViewHolder{
+        public static final int REQUEST_CALL = 1;
+        private TextView tbCurso;
+
+        public CursoHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tbCurso = itemView.findViewById(R.id.tbCurso);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (listener != null && position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(cursos.get(position));
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Curso curso);
+    }
+
+    public void setOnClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
 }
--- app/src/main/java/unis/edu/crudalunos/model/DisciplinaViewModelFactory.java
+++ app/src/main/java/unis/edu/crudalunos/model/DisciplinaViewModelFactory.java
@@ -1,4 +1,24 @@
 package unis.edu.crudalunos.model;
 
-public class DisciplinaViewModelFactory {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class DisciplinaViewModelFactory implements ViewModelProvider.Factory {
+
+    private Application application;
+    private int cursoId;
+
+    public DisciplinaViewModelFactory(Application application, int cursoId) {
+        this.application = application;
+        this.cursoId = cursoId;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new DisciplinaViewModel(application, cursoId);
+    }
 }
